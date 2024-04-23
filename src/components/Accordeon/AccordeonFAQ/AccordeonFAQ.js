ca2a5474d2d096f3522b5f211fb9abe27462ec7d
homo-errantium <homo.errantium@gmail.com>
2024-04-23 06:10:00 +0300

    changed slider

--- src/components/Accordeon/AccordeonFAQ/AccordeonFAQ.js
+++ src/components/Accordeon/AccordeonFAQ/AccordeonFAQ.js
@@ -2,77 +2,74 @@ import './AccordeonFAQ.sass';
 import vectorUp from '../../../images/vectorUp.svg';
 
 function AccordeonFAQ() {
-    return (
-        <div className='accordeon__container'>
-            <label className='accordion' htmlFor='services'>
-                <input type='radio' name='radio-accordion' id='services' />
-                <div className='accordion__header'>
-                    <p className='accordion__header-text'>
-                        Какие услуги предоставляет ваша студия?
-                    </p>
-                    <img
-                        src={vectorUp}
-                        alt='стрелка меню'
-                        className='accordion__header-img'
-                    />
-                </div>
-                <div className='accordion__content'>
-                    <p className='accordion__content-text'>
-                        Мы придерживаемся высоких стандартов качества и
-                        стремимся создавать уникальный дизайн, отвечающий
-                        потребностям и ожиданиям наших клиентов. Наши дизайнеры
-                        постоянно совершенствуют свои навыки и следят за новыми
-                        трендами в дизайне..
-                    </p>
-                </div>
-            </label>
-            <label className='accordion' htmlFor='uniq'>
-                <input type='radio' name='radio-accordion' id='uniq' />
-                <div className='accordion__header'>
-                    <p className='accordion__header-text'>
-                        Как вы обеспечиваете качество и уникальность дизайна?
-                    </p>
-                    <img
-                        src={vectorUp}
-                        alt='стрелка меню'
-                        className='accordion__header-img'
-                    />
-                </div>
-                <div className='accordion__content'>
-                    <p className='accordion__content-text'>
-                        Мы придерживаемся высоких стандартов качества и
-                        стремимся создавать уникальный дизайн, отвечающий
-                        потребностям и ожиданиям наших клиентов. Наши дизайнеры
-                        постоянно совершенствуют свои навыки и следят за новыми
-                        трендами в дизайне.
-                    </p>
-                </div>
-            </label>
-            <label className='accordion' htmlFor='content'>
-                <input type='radio' name='radio-accordion' id='content' />
-                <div className='accordion__header'>
-                    <p className='accordion__header-text'>
-                        Предоставляете ли вы услуги по созданию контента для
-                        сайтов и приложений?
-                    </p>
-                    <img
-                        src={vectorUp}
-                        alt='стрелка меню'
-                        className='accordion__header-img'
-                    />
-                </div>
-                <div className='accordion__content'>
-                    <p className='accordion__content-text'>
-                        Мы придерживаемся высоких стандартов качества и
-                        стремимся создавать уникальный дизайн, отвечающий
-                        потребностям и ожиданиям наших клиентов. Наши дизайнеры
-                        постоянно совершенствуют свои навыки и следят за новыми
-                        трендами в дизайне..
-                    </p>
-                </div>
-            </label>
+  return (
+    <div className="accordion__container">
+      <label className="accordion" htmlFor="services">
+        <input type="radio" name="radio-accordion" id="services" />
+        <div className="accordion__header">
+          <p className="accordion__header-text">
+            Какие услуги предоставляет ваша студия?
+          </p>
+          <img
+            src={vectorUp}
+            alt="стрелка меню"
+            className="accordion__header-img"
+          />
         </div>
-    );
+        <div className="accordion__content">
+          <p className="accordion__content-text">
+            Мы придерживаемся высоких стандартов качества и стремимся создавать
+            уникальный дизайн, отвечающий потребностям и ожиданиям наших
+            клиентов. Наши дизайнеры постоянно совершенствуют свои навыки и
+            следят за новыми трендами в дизайне..
+          </p>
+        </div>
+      </label>
+      <label className="accordion" htmlFor="uniq">
+        <input type="radio" name="radio-accordion" id="uniq" />
+        <div className="accordion__header">
+          <p className="accordion__header-text">
+            Как вы обеспечиваете качество и уникальность дизайна?
+          </p>
+          <img
+            src={vectorUp}
+            alt="стрелка меню"
+            className="accordion__header-img"
+          />
+        </div>
+        <div className="accordion__content">
+          <p className="accordion__content-text">
+            Мы придерживаемся высоких стандартов качества и стремимся создавать
+            уникальный дизайн, отвечающий потребностям и ожиданиям наших
+            клиентов. Наши дизайнеры постоянно совершенствуют свои навыки и
+            следят за новыми трендами в дизайне.
+          </p>
+        </div>
+      </label>
+      <label className="accordion" htmlFor="content">
+        <input type="radio" name="radio-accordion" id="content" />
+        <div className="accordion__header">
+          <p className="accordion__header-text">
+            Предоставляете ли вы услуги по созданию контента для сайтов и
+            приложений?
+          </p>
+          <img
+            src={vectorUp}
+            alt="стрелка меню"
+            className="accordion__header-img"
+          />
+        </div>
+        <div className="accordion__content">
+          <p className="accordion__content-text">
+            Мы придерживаемся высоких стандартов качества и стремимся создавать
+            уникальный дизайн, отвечающий потребностям и ожиданиям наших
+            клиентов. Наши дизайнеры постоянно совершенствуют свои навыки и
+            следят за новыми трендами в дизайне..
+          </p>
+        </div>
+      </label>
+    </div>
+  );
 }
 
 export default AccordeonFAQ;
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,118 +1,53 @@
+import React from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 import './Slider.sass';
-import { NavLink } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-// import Slide from './Slide/Slide';
-import sliderButtonLeft from '../../images/vectorLeft.svg';
-import sliderButtonRight from '../../images/vectorRight.svg';
 
-function Slider({ data }) {
-    const [slideWidth, setslideWidth] = useState(100);
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-    const [pages, setPages] = useState([]);
-    const [offset, setOffset] = useState(0);
+// Import Swiper styles
+import 'swiper/scss';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
-    function handleChangeCardWidth() {
-        switch (true) {
-            case window.innerWidth >= 1360:
-                setslideWidth(1360);
-                break;
-            case window.innerWidth < 1360:
-                setslideWidth(window.innerWidth);
-                break;
-            default:
-                setslideWidth(window.innerWidth);
-                break;
-        }
-    }
+// import required modules
+import { Pagination, Navigation } from 'swiper/modules';
+import { useNavigate } from 'react-router-dom';
 
-    useEffect(() => {
-        setWindowWidth(window.innerWidth);
-        handleChangeCardWidth();
-        window.addEventListener('resize', handleChangeCardWidth);
-        return () =>
-            window.removeEventListener('resize', handleChangeCardWidth);
-    }, [slideWidth, windowWidth]);
+export default function Slider({ data }) {
+  const navigate = useNavigate();
 
-    function handleBtnRedirect() {
-        <NavLink to='/news' replace={true} />;
-    }
-
-    function handleRightClick() {
-        setOffset(() => {
-            const newOffset = offset - slideWidth;
-            const maxOffset = -(slideWidth * (pages.length - 1));
-            return Math.max(newOffset, maxOffset);
-        });
-    }
-
-    function handleLeftClick() {
-        setOffset(() => {
-            const newOffset = offset + slideWidth;
-            return Math.min(newOffset, 0);
-        });
-    }
-
-    useEffect(() => {
-        const slides = data.map((slide) => (
-            <div key={slide.id} className='slide'>
-                <div className='slide__text-container'>
-                    <p className='slide__title'>{slide.title}</p>
-                    <p className='slide__description'>{slide.description}</p>
-                    {slide.date && <p className='slide__date'>{slide.date}</p>}
-                </div>
-                <img
-                    src={slide.image}
-                    alt={slide.title}
-                    className='slide__image'
-                />
-            </div>
-        ));
-        setPages(slides);
-    }, [windowWidth, data, slideWidth]);
-
-    return (
-        <div className='slider'>
-            <button
-                className='slider__button-website'
-                type='button'
-                onClick={handleBtnRedirect}
-            >
-                cайты
-            </button>
-            <div className='slider__window'>
-                <div
-                    className='slider__slides'
-                    style={{ transform: `translateX(${offset}px)` }}
-                >
-                    {pages}
-                </div>
-            </div>
-            <div className='slider__control-buttons'>
+  return (
+    <>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+      >
+        {data.map((item) => {
+          return (
+            <SwiperSlide key={item.id}>
+              <div className="swiper__text-container">
                 <button
-                    className='slider__button slider__button_left'
-                    type='button'
-                    onClick={handleLeftClick}
+                  type="button"
+                  className="swiper__website-button"
+                  onClick={() => navigate('one', { replace: false })}
                 >
-                    <img
-                        src={sliderButtonLeft}
-                        alt='стрелка слайдера'
-                        className='slider__button-image'
-                    />
+                  Сайты
                 </button>
-                <button
-                    className='slider__button slider__button_right'
-                    type='button'
-                    onClick={handleRightClick}
-                >
-                    <img
-                        src={sliderButtonRight}
-                        alt='стрелка слайдера'
-                        className='slider__button-image'
-                    />
-                </button>
-            </div>
-        </div>
-    );
+                <h2 className="swiper__title">{item.title}</h2>
+                <p className="swiper__description">{item.description}</p>
+                {item.date && <p className="swiper__date">{item.date}</p>}
+              </div>
+              <img
+                src={item.image}
+                alt={item.title}
+                className="swiper__image"
+              />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </>
+  );
 }
-
-export default Slider;
